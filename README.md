7d83f5fb5b7996b6ad404f39ed789639f8c92972
moskalyk <morgan.moskalyk@protonmail.ch>
2024-11-20 11:45:52 -0500

    updated with highlighting

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Anatomical SVG Drawing of Human Body Muscles Unisex
+# Unisex Muscle Anatomical SVG Drawing of Human Body
 
 Free to use
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,36 @@ import './App.css'
 import UnisexBody from './UnisexBody'
 
 function App() {
+    
+    const highlightColors = {
+        abs: "#15FFFF",
+        forearms: "#7A96FC",
+        knees: "#FD9494",
+        rhomboids: "#5CFC3B",
+        achilles: "#FB26D6",
+        glutes: "#362EFA",
+        lats: "#303030",
+        rotator_cuffs: "#000000",
+        biceps: "#34FC44",
+        hamstrings: "#2EFC97",
+        lower_back: "#FFE786",
+        shins: "#9F9F9F",
+        calfs: "#F9106C",
+        hands: "#003DFA",
+        neck: "#0099FB",
+        thighs: "#AC2EFB",
+        hips: "#FC7E32",
+        obliques: "#FFD52F",
+        traps: "#87FEFE",
+        deltoids: "#808080",
+        inner_thighs: "#FFB081",
+        pecs: "#FE0000",
+        triceps: "#BCFC3A",
+    }
+
   return (
     <>
-        <UnisexBody/>
+        <UnisexBody highlightColors={highlightColors} highlights={Object.keys(highlightColors)}/>
     </>
   )
 }
